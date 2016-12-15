bf2f6f9af5aaa62a897674140a92f13559d825b3
Tnplay <tnplayyoutube@yahoo.com.br>
2016-12-15 14:37:05 -0200

    Update online.js

--- js/online.js
+++ js/online.js
@@ -1,3 +1,13 @@
- //Mostra um pop-up com a versão oficial do WeekNews
   var Version = ("V 1.0.0");
-window.alert("Versão atual do WeekNews: " + Version);
+  var Online = true;
+  var On = ("Status: WeekNews ON");
+  var Off = ("Status: WeekNews OFF, volte mais tarde");
+  var Error = ("Erro critico");
+  if (Online === true) {
+   console.log(On);
+} else if(Online === false) {
+   console.log(Off);
+} else {
+  console.log(Error);
+}
+console.log("Verso atual do WeekNews: " + Version);
