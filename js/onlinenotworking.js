a08305a420907aba34fee668e4d1ea8fba5360a4
Tnplay <tnplayyoutube@yahoo.com.br>
2016-12-15 12:48:49 -0200

    Add files via upload

---
+++ js/onlinenotworking.js
@@ -0,0 +1,14 @@
+  var Version = ("V 1.0.0");
+  var Online = true;
+  var On = ("Status: WeekNews ON");
+  var Off = ("Status: WeekNews OFF, volte mais tarde");
+  if (Online === true) {
+   window.alert(On)
+} else {
+   window.alert(Off)
+   //não usar o seguinte código:
+   //close.()
+   //usar apenas com permissão do desenvolvedor
+}
+window.alert("Versão atual do WeekNews: " + Version);
+
