4deaf1d3a438762260fedb9e905d3410f6679a10
Tnplay <tnplayyoutube@yahoo.com.br>
2016-12-24 02:34:47 -0200

    Update javascript1.js

--- javascript1.js
+++ javascript1.js
@@ -8,7 +8,7 @@ var Version = ("V 1.0.0");
    window.alert(On);
 } else if(Online === false) {
    window.alert(Off);
-    <a href="https://tnplay.github.io/error404.github.io/"></a>
+    window.location.replace("https://tnplay.github.io/error404.github.io/");
    //código em fase de testes beta:
    //close.()
    //não usar sem permissão
